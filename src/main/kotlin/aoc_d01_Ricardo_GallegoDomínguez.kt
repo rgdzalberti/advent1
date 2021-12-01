a69fdd4409a7d511566d502e26f6c4c6a9ff95af
Ricardo <rgallego@iesrafaelalberti.es>
2021-12-02 00:17:30 +0100

    Terminado de verdad

--- src/main/kotlin/aoc_d01_Ricardo_GallegoDomínguez.kt
+++ src/main/kotlin/aoc_d01_Ricardo_GallegoDomínguez.kt
@@ -1,14 +1,31 @@
 import java.io.File
 
+val listaNumeros = mutableListOf<Int>()
+val TriplelistaNumeros = mutableListOf<Int>()
+val newList = mutableListOf<Int>()
+
+fun Incremento(listaNumeros: List<Int>): Int{
+    var resultado = 0;
+    try {for (i in 0..listaNumeros.size) { if (listaNumeros[i] < listaNumeros[i + 1]) {resultado++}  } } catch (e: IndexOutOfBoundsException) { }
+    return resultado
+}
+
+
 fun main() {
 
     var file: File = File("C:\\Users\\Ricar\\Desktop\\Nueva carpeta\\input.txt")
-    var contador: Int = 0;
-    var contador2: Int = 1;
-    var resultado: Int = 0
-    val listaNumeros = mutableListOf<Int>()
 
-    file.forEachLine { listaNumeros.add(it.toInt()) }
-    try { for (i in 0..listaNumeros.size) { if (listaNumeros[contador] < listaNumeros[contador2]) { contador++;contador2++;resultado++ } else { contador++;contador2++ } } }
-    catch (e: IndexOutOfBoundsException) { println(resultado) }
+
+    file.forEachLine { listaNumeros.add(it.toInt())}
+    file.forEachLine { TriplelistaNumeros.add(it.toInt())}
+
+    println(Incremento(listaNumeros))
+
+    try { for (i in 0..TriplelistaNumeros.size) { val value = TriplelistaNumeros[i] + TriplelistaNumeros[i + 1] + TriplelistaNumeros[i + 2]; newList.add(value)} } catch (e: IndexOutOfBoundsException) { }
+    println(Incremento(newList))
+
+
+
+
+
 }
